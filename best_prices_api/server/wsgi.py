1ce604f750b16de0807cf883d16d93a28b802b2a
teenoh <ugbalaachinike@gmail.com>
2019-05-11 21:18:28 +0100

    bug fixes

--- best_prices_api/server/wsgi.py
+++ best_prices_api/server/wsgi.py
@@ -13,17 +13,14 @@ import sys
 import site
 
 site.addsitedir('/home/teenoh/.virtualenvs/best_prices_venv/lib/python3.5/site-packages')
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-
 activate_this = os.path.expanduser("~/.virtualenvs/best_prices_venv/bin/activate_this.py")
-
 exec(open(activate_this).read())
-project = '/home/teenoh/webapps/best_prices/best_prices/best_prices_api/server/'
+project = '/home/teenoh/webapps/best_prices/best_prices/best_prices_api/'
 workspace = os.path.dirname(project)
 sys.path.append(workspace)
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 application = get_wsgi_application()
