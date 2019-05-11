f199f324698f34155ba06ad171ca5c344172a82d
teenoh <ugbalaachinike@gmail.com>
2019-05-11 17:44:28 +0100

    add requirements.txt file

--- best_prices_api/server/wsgi.py
+++ best_prices_api/server/wsgi.py
@@ -1,3 +1,4 @@
+from django.core.wsgi import get_wsgi_application
 """
 WSGI config for server project.
 
@@ -8,9 +9,17 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
 
-from django.core.wsgi import get_wsgi_application
+site.addsitedir('/home/teenoh/.virtualenvs/mino/lib/python3.5/site-packages')
+activate_this = os.path.expanduser("~/.virtualenvs/mino/bin/activate_this.py")
+exec(open(activate_this).read())
+project = '/home/teenoh/webapps/minomain/mino/'
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
