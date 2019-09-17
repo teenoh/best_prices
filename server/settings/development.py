894818f6eed9ab80dea3ae8c610ba73d5ff6dee9
teenoh <ugbalaachinike@gmail.com>
2019-09-17 22:08:07 +0100

    update requirements.txt

--- server/settings/development.py
+++ server/settings/development.py
@@ -9,3 +9,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
