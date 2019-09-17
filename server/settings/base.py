7e0655f3ccb538f1f979891a018e6bfeb41928f3
teenoh <ugbalaachinike@gmail.com>
2019-09-17 21:52:09 +0100

    update settings

--- server/settings/base.py
+++ server/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.elasticbeanstalk.com', 'best-prices.us-west-2.elasticbeanstalk.com', 'develop.upcqqqxpap.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
