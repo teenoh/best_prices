07f55e7684083574b206a345287673d735c59e95
teenoh <ugbalaachinike@gmail.com>
2019-09-17 20:27:45 +0100

    update code

--- best_prices_api/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import handle_item_post
-
-urlpatterns = [
-    path('post-item/', handle_item_post),
-]
--- server/wsgi.py
+++ server/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from .environment import SETTINGS_MODULE
+from server.environment import SETTINGS_MODULE
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', SETTINGS_MODULE)
